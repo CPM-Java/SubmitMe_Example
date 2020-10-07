7a1a83fd870d635dcce2bb41c44629cddf33a203
Kodi <cam.peterson@protonmail.com>
2020-10-07 10:41:36 -0500

    Init

---
+++ FirstClassEver.java
@@ -0,0 +1,28 @@
+
+ /**
+  Comments are shown in blue or gray.
+  Comments are not read by the compiler but show important
+   information for programmers
+  
+  @author YOUR NAME HERE 
+   @version DATE
+ */
+public class FirstClassEver
+{
+    // instance identifiers show what the object will remember
+    private int myNum = 28;
+
+
+    /**
+     * An example of a method.
+     * Methods allow objects to perform tasks when asked.
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int num)
+    {
+        // a very simple calculation
+        return myNum + num;
+    }
+}
